0f543c329943fd370cf6eee6477b27e16e2396c4
Lukáš Tomek <posli.posli@email.cz>
2023-03-05 17:54:13 +0100

    Update Basics.sql

--- Basics.sql
+++ Basics.sql
@@ -1,4 +1,11 @@
 -- Single line comment
 
 /* Multi-line comment
-*/
+ */
+ 
+ /* ----- BASIC INFORMATION -----
+    • database, tables, columns, rows
+    • PRIMARY KEYS = a column with a unique values (no NULL values) like an ID column (values 1, 2, 3, ...)
+    
+ 
+ 
